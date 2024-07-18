5b619d2075d566e7316603f405f6f1765e1fb6a3
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-07-18 14:12:14 +0500

    Working

---
+++ script.js
@@ -0,0 +1,8 @@
+ // Initialize AOS for animations
+ AOS.init();
+        
+ // Toggle mobile menu
+ function toggleMobileMenu() {
+     var mobileMenu = document.getElementById('mobileMenu');
+     mobileMenu.classList.toggle('hidden');
+ }
