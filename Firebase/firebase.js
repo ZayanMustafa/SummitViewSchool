14862691ef77d6442455a017de154a4279c1f03c
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-07-22 17:05:36 +0500

    Working

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -1,21 +1,36 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
+  import { getAuth ,
+    onAuthStateChanged,
+          } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAkQtQsZ6VoJEfa86G_NaCPK-cmXdOv5Iw",
-  authDomain: "firsteducationlwebsite.firebaseapp.com",
-  projectId: "firsteducationlwebsite",
-  storageBucket: "firsteducationlwebsite.appspot.com",
-  messagingSenderId: "757131881060",
-  appId: "1:757131881060:web:e451a4f1e4b6df46cd8d71",
-  measurementId: "G-21G3G4LZBX"
-};
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAkQtQsZ6VoJEfa86G_NaCPK-cmXdOv5Iw",
+    authDomain: "firsteducationlwebsite.firebaseapp.com",
+    projectId: "firsteducationlwebsite",
+    storageBucket: "firsteducationlwebsite.appspot.com",
+    messagingSenderId: "757131881060",
+    appId: "1:757131881060:web:85f54bcc57eb442ecd8d71",
+    measurementId: "G-ZV87DCV7MP"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  console.log(app)
+
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+// Export From FireBase 
+export{
+  getAuth,
+  onAuthStateChanged,
+}
--- script.js
+++ script.js
@@ -1,22 +1,39 @@
+import{ getAuth , onAuthStateChanged } from "./Firebase/firebase.js"
 
-    AOS.init();
-    // Counter Animation
-       $(window).on('scroll', function() {
-        $('.counter').each(function() {
-            var $this = $(this);
-            var countTo = $this.attr('data-target');
-            if ($(window).scrollTop() + $(window).height() > $this.offset().top && !$this.hasClass('counting')) {
-                $this.addClass('counting');
-                $({ countNum: $this.text() }).animate({ countNum: countTo }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function () {
-                        $this.text(Math.floor(this.countNum));
-                    },
-                    complete: function () {
-                        $this.text(this.countNum);
-                    }
-                });
-            }
+    // AOS.init();
+    // // Counter Animation
+    //    $(window).on('scroll', function() {
+    //     $('.counter').each(function() {
+    //         var $this = $(this);
+    //         var countTo = $this.attr('data-target');
+    //         if ($(window).scrollTop() + $(window).height() > $this.offset().top && !$this.hasClass('counting')) {
+    //             $this.addClass('counting');
+    //             $({ countNum: $this.text() }).animate({ countNum: countTo }, {
+    //                 duration: 2000,
+    //                 easing: 'swing',
+    //                 step: function () {
+    //                     $this.text(Math.floor(this.countNum));
+    //                 },
+    //                 complete: function () {
+    //                     $this.text(this.countNum);
+    //                 }
+    //             });
+    //         }
+    //     });
+    //     }).trigger('scroll');
+        
+        // Check if user is already SignIn or not
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+           console.log("User is hear")
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            // ...
+          } else {
+           console.log("User is not hear")
+            // User is signed out
+            // ...
+          }
         });
-        }).trigger('scroll');
