40d521b7520c584f834119c334087d75a64cdb1b
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-07-26 10:09:35 +0500

    Working

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-import{ getAuth , onAuthStateChanged } from "./Firebase Auth/firebase.js"
+// Import necessary functions from Firebase
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-        // Check if user is already SignIn or not
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-          if (user) {
-           console.log("User is hear")
-           window.location.href = './Registered User/registeredUser.html';
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/auth.user
-            const uid = user.uid;
-            // ...
-          } else {
-           console.log("User is not hear")
-            // User is signed out
-            // ...
-          //  window.location.href = '/index.html';
+// Check if user is already signed in or not
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User is here");
 
-          }
-         });
-        
+    // Check if the user's email is admin@gmail.com
+    if (user.email === 'admin@gmail.com') {
+      window.location.href = './Admin/admin.html';
+    } else {
+      window.location.href = './Registered User/registeredUser.html';
+    }
+    // Optional: You can access other user details here
+    const uid = user.uid;
+    // ...
+  } else {
+    console.log("User is not here");
+  }
+});
