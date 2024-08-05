f7bc54abea916b2755325f0abcc8495736bc6a77
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-08-05 16:44:05 +0500

    Google Authencation

--- Authencation/For Signup/signup.js
+++ Authencation/For Signup/signup.js
@@ -1,5 +1,5 @@
 // Import Firebase methods and elements
-import { auth, createUserWithEmailAndPassword, getAuth } from "../../Firebase Auth/firebase.js";
+import { auth, createUserWithEmailAndPassword,  getAuth, signInWithPopup, GoogleAuthProvider , provider } from "../../Firebase Auth/firebase.js";
 import { setDoc, doc, db,  } from "../../Firebase Auth/firebase.js";
 
 
@@ -57,5 +57,25 @@ googleSignUp.addEventListener("click" , accountByGoogle )
 
 function accountByGoogle(){
 
-  alert("Please Wait")
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+    window.location.href = "../../Registered User/registeredUser.html"
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+    // alert("Please Wait")
 }
--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// Import necessary functions from Firebase
-import { app, getAuth ,onAuthStateChanged } from "/Firebase Auth/firebase.js";
+  // Import necessary functions from Firebase
+  import { app, getAuth ,onAuthStateChanged } from "/Firebase Auth/firebase.js";
 
 
  const auth = getAuth(app);
- console.log("User is not here");
 
   // Check if user is already signed in or not
   onAuthStateChanged(auth, (user) => {
   if (user) {
-    console.log("User is here");
-    const uid = user.uid;
 
+    const uid = user.uid;
     if (user.email === "admin@gmail.com") {
       window.location.href = "../../Admin/admin.html";
     } else {
       window.location.href = "../../Registered User/registeredUser.html";
     }
   } else {
-
+      // Home page 
   }
   });
