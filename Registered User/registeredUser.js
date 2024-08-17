dabbdad84278a5bb7f14915cbda0c8bcc2a943e2
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-08-17 12:07:06 +0500

    Updated

--- Registered User/registeredUser.js
+++ Registered User/registeredUser.js
@@ -4,48 +4,38 @@ const auth = getAuth();
 const database = getDatabase();
 
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in
-    const userRef = databaseRef(database, 'UserData/' + user.uid);
-
+  const userRef = databaseRef(database, 'UserData/' + user.uid);
     console.log(user.uid, "user");
-
-    get(userRef)
+     get(userRef)
       .then((snapshot) => {
         if (snapshot.exists()) {
           const userData = snapshot.val();
           console.log("User Data Retrieved: ", userData);
-          // Call a function to update the UI with the user data
-          displayUserData(userData)
-        } else {
-          console.log("No data available");
+
+         displayUserData(userData)
         }
       })
       .catch((error) => {
         console.error("Error fetching user data:", error);
+        onAuthStateChanged()
       });
-  } else {
-    console.log("No user is signed in");
-  }
-});
+  });
   
 function displayUserData(userData) {
   console.log("Displaying user data: ", userData);
-
+  
   document.getElementById('studentName').textContent = userData.firstName + " " + userData.lastName;
   document.getElementById('profilePicture').src = userData.profileImageUrl;
 }
 
-
-
- // Import Elements from html 
- document.getElementById('logOutBtn').addEventListener('click', () => {
+// LogOut
+document.getElementById('logOutBtn').addEventListener('click', () => {
      const auth = getAuth();
      signOut(auth).then(() => {
-       // Sign-out successful.
+
        alert("Sign-out successful")
        window.location.href = "../index.html"
      }).catch((error) => {
-       // An error happened.
-     });
+      alert("Face Problem in SignOut")
+    });
  }); 
